a227a2f3f1f4756687d7bf127f22cc6772e0482a
InnaGolomoza <154058061+InnaGolomoza@users.noreply.github.com>
2023-12-21 12:23:58 +0200

    Update lab2.2.py

--- lab2.2.py
+++ lab2.2.py
@@ -8,10 +8,7 @@ day = int(input("Введіть день: "))
 month = int(input("Введіть місяць: "))
 year = int(input("Введіть рік: "))
 
-days_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] # Визначення кількості днів у місяці
 leap_year = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0) # Перевірка високосного року
-if leap_year:
-    days_month[2] = 29
 
 if not(1 <= day <= 31) or not (1 <= month <= 12) or year < 1: #Перевірка чи коректні дані
     print("Введені дані некоректні.")
@@ -22,8 +19,17 @@ else:
     if month == 0:
         month = 12
         year -= 1
-    day = days_month[month]
-
+    if day == 0:
+        if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+            day = 31
+        elif month == 4 or month == 6 or month == 9 or month == 11:
+            day = 30
+        else:
+            if leap_year:
+                day = 29
+            else:
+                day = 28
+                    
     # Виведення результату
     print(f"Вчорашня дата: {day:02d}.{month:02d}.{year:04d}")
    
